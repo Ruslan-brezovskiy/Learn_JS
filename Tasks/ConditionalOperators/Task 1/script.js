36c5b84a6ecfb35bed5aa60fb007699ed6ce4c5f
Ruslan-brezovskiy <lloxness98@mail.ru>
2018-06-15 14:40:49 +0300

    add readme and script

---
+++ Tasks/ConditionalOperators/Task 1/script.js
@@ -0,0 +1,8 @@
+var name = prompt(" Каково «официальное» название JavaScript? ", " ");
+
+if (name == "ECMAScript") {
+	alert (" Верно! ");
+}
+else {
+	alert (" Не знаете? «ECMAScript»! ");
+}
---
+++ Tasks/ConditionalOperators/Task 2/script.js
@@ -0,0 +1,11 @@
+var value = prompt( " Введите число ", " " );
+
+if (value > 0) {
+	alert( 1 );
+}
+else if (value < 0) {
+	alert( -1 );
+}
+else{
+	alert( 0 );
+}
